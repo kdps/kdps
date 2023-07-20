e1c523afdf3f4f95f95196fb1d91154c4d06691f
kdp <kdp7584@gmail.com>
2023-07-20 11:21:02 +0900

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -6,8 +6,26 @@ obj = wave.open('sound.wav','w')
 obj.setnchannels(1) # mono
 obj.setsampwidth(2)
 obj.setframerate(sampleRate)
+
 for i in range(99999):
-   value = random.randint(-32767, 32767)
+   value = random.randint(10000, 10000)
    data = struct.pack('<h', value)
    obj.writeframesraw( data )
+
+for i in range(99999):
+   value = random.randint(1000, 2000)
+   data = struct.pack('<h', value)
+   obj.writeframesraw( data )
+
+for i in range(99999):
+   value = random.randint(300, 400)
+   data = struct.pack('<h', value)
+   obj.writeframesraw( data )
+
+
+for i in range(99999):
+   value = random.randint(0, 100)
+   data = struct.pack('<h', value)
+   obj.writeframesraw( data )
+   
 obj.close()
