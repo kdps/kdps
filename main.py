b233992b1c7b96ccb60e510e119a902882f0da41
kdp <kdp7584@gmail.com>
2023-07-20 14:24:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def generateAudio(frequence: audioParameter):
 
   for hz in frequence.frequences:
     for i in range(99999):
-       value = random.randint(0, 500)
+       value = random.randint(int(hz), int(hz) + (int(hz) // 2))
        data = struct.pack('<h', value)
        obj.writeframesraw( data )
   
