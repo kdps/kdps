96b812fea51b80f41f17c2f301069066fe2d5634
kdp <kdp7584@gmail.com>
2023-07-24 18:00:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -146,30 +146,103 @@ def generateAudio(frequence: List[audioParameter]):
   return list_names
 
 
-def read_wav(filename):
-    # open file, get metadata for audio
-    try:
-        wf = wave.open(filename, "rb")
-    except IOError as e:
-        print(e)
-        return
 
-    # typ = choose_type( wf.getsampwidth() ) # TODO: implement choose_type
-    nsamps = wf.getnframes()
-    assert nsamps > 0
 
-    fs = wf.getframerate()
-    assert fs > 0
 
-    # Read entire file and make into an array
-    samps = list(array.array("i", wf.readframes(nsamps)))
 
-    try:
-        assert nsamps == len(samps)
-    except AssertionError:
-        print(nsamps, "not equal to", len(samps))
 
-    return samps, fs
+
+
+
+
+
+from scipy.io import wavfile
+from scipy.signal import resample
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import pandas
+
+sys.path.append('../')
+import heartpy as hb
+
+__author__ = "Paul van Gent"
+__version__ = "Version 0.9"
+
+def read_wav(file):
+    '''Function reads file and outputs sample rate and data array'''
+    samplerate, wv = wavfile.read(file)
+    wv_data = np.array(wv, dtype=np.float32)
+    return samplerate, wv_data
+
+def mark_audio_peaks(peaklist, samplerate):
+    '''
+    Marks peaks in audio signal, implements crude type detection
+
+    Keyword arguments:
+    peaklist -- 1-dimensional list or array containing detected peak locations
+    samplerate -- sample rate of the original signal
+    '''
+
+    hb.calc_rr(samplerate)
+    first_heartsounds = []
+    second_heartsounds = []
+    rejected = []
+
+    cnt = 0
+    for rr in hb.working_data['RR_list']:
+        if rr <= 0.6*np.mean(hb.working_data['RR_list']):
+            rejected.append(peaklist[cnt])
+        elif rr >= 0.9*np.mean(hb.working_data['RR_list']):
+            first_heartsounds.append(peaklist[cnt])
+        else:
+            second_heartsounds.append(peaklist[cnt])
+        cnt += 1
+
+    hb.working_data['peaklist'] = first_heartsounds
+    hb.working_data['second_heartsounds'] = second_heartsounds
+    hb.working_data['removed_beats'] = rejected
+
+def plotter(show=True, title='Heart Rate Audio Signal Peak Detection'):
+    '''Alternative plotter function to plot signals'''
+
+    plt.title(title)
+    plt.plot(abs(hb.working_data['hr']))
+    plt.plot(hb.working_data['rolmean'])
+    plt.scatter(hb.working_data['peaklist'], [hb.working_data['hr'][x] for x in hb.working_data['peaklist']], color='green', label='first heartsounds')
+    plt.scatter(hb.working_data['second_heartsounds'], [hb.working_data['hr'][x] for x in hb.working_data['second_heartsounds']], color='black', label='second heartsounds')
+    plt.scatter(hb.working_data['removed_beats'], [hb.working_data['hr'][x] for x in hb.working_data['removed_beats']], color='red', label='rejected peaks')
+    plt.legend()
+    plt.show()
+
+def process(filename):
+    '''Processes the wav file passed to it
+
+    Keyword arguments:
+    filename -- absolute or relative path to WAV file
+    '''
+
+    print('resampling signal to 1000Hz')
+    samplerate, wv_data = read_wav(filename)
+    wv_data = abs(wv_data)
+    wavlength = len(wv_data) / samplerate
+
+    wv_data = resample(wv_data, int(1000*wavlength))
+    new_samplerate = len(wv_data) / wavlength
+
+    print('detecting peaks')
+
+    df = pandas.DataFrame(wv_data)
+    df.to_csv("sound.csv", sep=',',index=False)
+
+    data = hb.get_data('sound.csv')
+
+    working_data, measures = hb.process(data, new_samplerate)  # 100 HZ
+
+    return measures['bpm']
+
+
+
 
 
 @app.post("/bpm_api")
@@ -197,77 +270,20 @@ async def bpm_api(file: UploadFile):
 
 
     # --------------------------------------------------------------------------------
+    samplerate, wv_data = read_wav(filename)
+    wv_data = abs(wv_data)
+    wavlength = len(wv_data) / samplerate
 
-    samps, fs = read_wav(dst)
+    wv_data = resample(wv_data, int(1000*wavlength))
+    new_samplerate = len(wv_data) / wavlength
 
+    print('detecting peaks')
 
-    # print(len(samps))
-    # print(fs/1000)
-    HZ = fs/1000
-
-    df = pandas.DataFrame(samps)
+    df = pandas.DataFrame(wv_data)
     df.to_csv("sound.csv", sep=',',index=False)
 
-    # --------------------------------------------------------------------------------
+    data = hb.get_data('sound.csv')
 
-
-    data = hp.get_data('sound.csv')
-
-    working_data, measures = hp.process(data, HZ)  # 100 HZ
-    # hp.plotter(working_data, measures)
-    # hp.plotter(working_data, measures, show = True, title = 'Heart Rate Signal Peak Detection')
-
-    os.remove(src)
-    os.remove(dst)
-    os.remove('sound.csv')
+    working_data, measures = hb.process(data, new_samplerate)  # 100 HZ
 
     return measures['bpm']
-    # print(measures['bpm']) #returns BPM value
-
-
-    # plot_object = hp.plotter(working_data, measures, show=False)
-    # plot_object.savefig('plot_1.jpg') #saves the plot as JPEG image.
-
-
-@app.post("/bpm_parse")
-def bpmParse():
-  filename = ""
-  
-  samps, fs = read_wav(filename)
-  data = []
-  correl = []
-  bpm = 0
-  n = 0
-  nsamps = len(samps)
-  window_samps = int(args.window * fs)
-  samps_ndx = 0  # First sample in window_ndx
-  max_window_ndx = math.floor(nsamps / window_samps)
-  bpms = numpy.zeros(max_window_ndx)
-
-  # Iterate through all windows
-  for window_ndx in range(0, max_window_ndx):
-
-      # Get a new set of samples
-      # print(n,":",len(bpms),":",max_window_ndx_int,":",fs,":",nsamps,":",samps_ndx)
-      data = samps[samps_ndx : samps_ndx + window_samps]
-      if not ((len(data) % window_samps) == 0):
-          raise AssertionError(str(len(data)))
-
-      bpm, correl_temp = bpm_detector(data, fs)
-      if bpm is None:
-          continue
-      bpms[window_ndx] = bpm
-      correl = correl_temp
-
-      # Iterate at the end of the loop
-      samps_ndx = samps_ndx + window_samps
-
-      # Counter for debug...
-      n = n + 1
-
-  bpm = numpy.median(bpms)
-  print("Completed!  Estimated Beats Per Minute:", bpm)
-
-  n = range(0, len(correl))
-  plt.plot(n, abs(correl))
-  plt.show(block=True)
